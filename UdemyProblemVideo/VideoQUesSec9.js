45588a077b1f02bc51de76e988c589f47a61b3e5
Leo Jusme <leojusme@gmail.com>
2023-09-07 21:39:24 -0400

    Practice problem from video

---
+++ UdemyProblemVideo/VideoQUesSec9.js
@@ -0,0 +1,34 @@
+//Instructions are insert true and false keys from question map in the prompt
+
+//Populating a Map without using SET METHOD
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try again!"],
+]);
+console.log(question);
+
+//Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number")
+    console.log(`Answer 
+  ${key}: ${value}`);
+}
+
+//My answer to Challenge of putting keys true and false in prompt answer
+const answer = Number(prompt("Your answer"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(true);
+  if (typeof key !== "number") console.log(false);
+}
+console.log(answer);
