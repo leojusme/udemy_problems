87262711c1e322a953a3f649a08b698d84ea7603
Leo Jusme <leojusme@gmail.com>
2023-10-01 17:10:03 -0400

    answer

--- Sec11-4.js
+++ Sec11-4.js
@@ -27,3 +27,52 @@ const dogs = [
 
 GOOD LUCK 😀
 */
+
+//1.
+dogs.forEach((dog) => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+console.log(dogs);
+
+//2.
+const dogSarah = dogs.find((dog) => dog.owners.includes("Sarah"));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating ${
+    dogSarah.curFood > dogSarah.recFood ? "much" : "little"
+  }`
+);
+
+//3.
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.curFood > dog.recFood)
+  .flatMap((dog) => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter((dog) => dog.curFood < dog.recFood)
+  .flatMap((dog) => dog.owners);
+console.log(ownersEatTooLittle);
+
+//4.
+//"Matilda and Alice and Bob's dogs eat too much!"
+//"Sarah and John and Michael's dogs eat too little!"
+
+console.log(`${ownersEatTooMuch.join(" and ")}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(" and ")}'s dogs eat too little!`);
+
+//5.
+console.log(dogs.some((dog) => dog.curFood === dog.recFood));
+
+//6.
+//current > (recommended * 0.90) && current < (recommended * 1.10)
+const checkEatingOkay = (dog) =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay));
+
+//7.
+console.log(dogs.filter(checkEatingOkay));
+
+// 8.
+//sort it by recommended food portion in an ascending order
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
